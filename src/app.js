5e85ae9839095233a6c82eadd3e6f6620440eb6c
ewalker24 <114313334+ewalker24@users.noreply.github.com>
2022-10-22 16:41:37 -0700

    Time

--- src/app.js
+++ src/app.js
@@ -1,16 +1,39 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperature = (document.querySelector(".temperature").innerHTML =
-    response.temperature.current);
-  let city = (document.querySelector(".city").innerHTML = response.city);
+    Math.round(response.data.temperature.current));
+  let city = (document.querySelector(".city").innerHTML = response.data.city);
   let description = (document.querySelector("#description").innerHTML =
-    response.condition.description);
+    response.data.condition.description);
   let humidity = (document.querySelector(".humidity").innerHTML =
-    response.temperature.humidity);
-  let wind = (document.querySelector(".wind").innerHTML = response.wind.speed);
+    "Humidity: " + response.data.temperature.humidity + "%");
+  let wind = (document.querySelector(".wind").innerHTML =
+    "Wind: " + Math.round(response.data.wind.speed) + "mph");
+  let date = document.querySelector(".date");
+  date.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let apiKey = "047ff5f243b184d84367a2f1o1cta7f9";
 let units = "imperial";
-let city = "San Francisco";
+let city = "London";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
 axios.get(apiUrl).then(displayTemperature);
